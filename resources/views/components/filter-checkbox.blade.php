74130dcfd4cad4580d993382bbd12d0661b180b6
Raleigh Groesbeck <raleighgroesbeck@gmail.com>
2023-06-09 08:14:08 -0500

    Corrected SELECT ALL behavior after submitting form

--- resources/views/components/filter-checkbox.blade.php
+++ resources/views/components/filter-checkbox.blade.php
@@ -4,7 +4,7 @@
     <li class="text-left">
         <input type="hidden" name="filter-checkbox[0]" value="0">
         <input type="checkbox" id="filter-checkbox-select-all" name="filter-checkbox[]" value="BOOK"
-        @checked($selected == null)
+        @checked($selected == null || !in_array(0, $selected))
         >
         <label for="filter-checkbox-select-all"> SELECT ALL</label>
     </li>
